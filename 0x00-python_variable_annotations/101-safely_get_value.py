aede450205ed2649f3f7e8feb5145461e6215d49
gideon <kipkerring.gedion@gmail.com>
2023-10-05 14:28:33 +0300

    application of TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Application of TypeVar"""
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)
+                     -> Union[Any, T]:
+    """Use of TypeVar"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
