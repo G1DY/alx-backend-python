d885867abdb2ec4418a4b88720fcf7dde5084e90
gideon <kipkerring.gedion@gmail.com>
2023-10-05 11:59:21 +0300

    function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes str1 and str2 and concatenates them to one string"""
+    return str1 + str2
