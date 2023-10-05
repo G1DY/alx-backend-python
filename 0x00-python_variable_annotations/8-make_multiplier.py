4473aca0104d7693be675da67472cef09fc13932
gideon <kipkerring.gedion@gmail.com>
2023-10-05 13:04:39 +0300

    a type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier.

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function make_multiplier that takes a float multiplier as argument
+   and returns a function that multiplies a float by multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """a callable function that returns a float"""
+    def new_function(x: float) -> float:
+        return x * multiplier
+    return new_function
