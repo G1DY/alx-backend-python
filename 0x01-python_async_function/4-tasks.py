96a7cc2a8a64d473c0373513d734b3bcbef00687
gideon <kipkerring.gedion@gmail.com>
2023-10-13 10:10:03 +0300

    spawns task_wait_random n times

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Imports task_wait_random from the previous python file
+   writes an async routine called wait_n that takes
+   in 2 int arguments
+   """
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('0-basic_async_syntax').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """spawns task_wait_random n times"""
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
